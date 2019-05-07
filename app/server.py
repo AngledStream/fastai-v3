a73def7fc8cb5274ef62762344e550815cc7bcc4
AngledStream <AngledStream@users.noreply.github.com>
2019-05-07 08:22:13 -0700

    shareable link into download link

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/open?id=18x1uc8_tN1rjNtqBCrQFbj-cI_VxwTdn'
+export_file_url = 'https://drive.google.com/uc?export=download&id=18x1uc8_tN1rjNtqBCrQFbj-cI_VxwTdn'
 export_file_name = 'vehicle_classifier.pkl'
 
 classes = ['bus', 'sedan', 'truck']
